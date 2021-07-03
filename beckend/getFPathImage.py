507ef4cfbc61f37964d7146d0bb351f1f94cf05f
Bangtx <bangtran.hha@gmail.com>
2021-07-03 09:04:48 +0700

    Initial commit

--- beckend/getFPathImage.py
+++ beckend/getFPathImage.py
@@ -1,4 +1,5 @@
 import os, sqlite3
+from builtins import type
 from typing import re
 
 db = sqlite3.connect('db.sqlite3')
@@ -73,13 +74,24 @@ def insert_one_question(**kwargs):
                 '{kwargs['document']}', 
                 '{kwargs['question']}', 
                 '{kwargs['type']}', 
-                '{kwargs['link']}')"""
+                '{kwargs['link']}')
+                """
     cursor.execute(sql)
     db.commit()
 
 
 def is_file(fileName):
-    if fileName.find('.png') != -1:
+    if len(fileName) == 11:
+        if fileName.find('.png') != -1:
+            return True
+        else:
+            return False
+    else:
+        return False
+
+
+def is_question(file_name):
+    if len(file_name) == 11:
         return True
     else:
         return False
@@ -124,7 +136,6 @@ def is_multi_choice(name):
         if name[name.index('.png') - 1: name.index('.png')] != '_':
             print(name[name.index('.png') - 1: name.index('.png')] )
             return True
-
     return False
 
 
@@ -132,11 +143,11 @@ def is_long_response(name):
     return not is_multi_choice(name)
 
 
-print(is_multi_choice('C0201M_.png'))
-print(is_long_response('C0201M_.png'))
+def ans_multi_choice(name):
+    return name[name.index('.png') - 1: name.index('.png')]
 
 
-def main():
+def begin():
 
     list_subject_id = list()
     list_topic_id = list()
@@ -156,20 +167,60 @@ def main():
     for doc in list_doc:
         list_document_id.append(insert_one('document', doc))
 
+
+def get_data():
+    list_subject = select_all('subject')
+    list_topic = select_all('topic')
+    list_doc = select_all('document')
+
     list_question_id = list()
     for subjec in list_subject:
         for topic in list_topic:
-            list_doc_question = os.listdir(f'question/{subjec}/{topic}')
+            list_doc_question = os.listdir(f"question/{subjec[1]}/{topic[1]}")
             for doc_and_file in list_doc_question:
-                if is_file(doc_and_file):
-                    insert_one_question(subject=subjec, topic=topic, doc='', )
-                    # list_question_id.append(insert_one('question', doc_and_file))
-                # print(topic)
-                # print(os.listdir(f'question/{subjec}/{topic}/{doc}'))
-                # list_name_2 += os.listdir(f'question/{subjec}/{topic}/{doc}')
+                if is_file(doc_and_file) and is_question(doc_and_file):
+                    doc = ''
+                    if is_multi_choice(doc_and_file):
+                        type = 'multi choice'
+                    if is_long_response(doc_and_file):
+                        type = 'long response'
+                    # insert_one_question(
+                    #     subject=subjec[0],
+                    #     topic=topic[0],
+                    #     document=doc,
+                    #     question=doc_and_file,
+                    #     type=type,
+                    #     link=f'question/{subjec[1]}/{topic[1]}/{doc}/{doc_and_file}'
+                    # )
+                    print(f'question/{subjec[1]}/{topic[1]}/{doc}/{doc_and_file}')
+                    print(not is_file(doc_and_file))
+                if not is_file(doc_and_file):
+                    for i in list_doc:
+                        if doc_and_file == i[1]:
+                            doc_and_file = i[1]
+                        # print(f'question/{subjec[1]}/{topic[1]}/{doc_and_file}')
+                        list_question = os.listdir(f'question/{subjec[1]}/{topic[1]}')
+                        for question in list_question:
+                            if is_file(question) and is_question(question):
+                                print(f'question/{subjec[1]}/{topic[1]}/{doc_and_file}')
+                                if is_multi_choice(question):
+                                    type = 'multi choice'
+                                if is_long_response(question):
+                                    type = 'long response'
+                                # insert_one_question(
+                                #     subject=subjec[0],
+                                #     topic=topic[0],
+                                #     document=doc_and_file,
+                                #     question=doc_and_file,
+                                #     type=type,
+                                #     link=f'question/{subjec[1]}/{topic[1]}/{doc}/{doc_and_file}'
+                                # )
+
 
 create_database(list_sql)
-# main()
+# begin()
+get_data()
+# begin()
 # print(select_all('topic'))
 # print(select_one('topic', 3))
 # data = insert_one('subject', 'hoa hoc')
